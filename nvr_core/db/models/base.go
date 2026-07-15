10132754dd273af915a2b24ed34f94966b962d94
Taniro Jang <tan.low@gmail.com>
2026-07-15 16:34:16 +0800

    - [nvr_core][db] Minor update.

--- nvr_core/db/models/base.go
+++ nvr_core/db/models/base.go
@@ -1,3 +1,9 @@
 package models
 
+import "time"
+
 type PartialUpdateInterfaces map[string]interface{}
+
+func (pui PartialUpdateInterfaces) ApplyUpdatedAt() {
+	pui["updated_at"] = time.Now().Unix()
+}
--- nvr_core/db/repository/base.go
+++ nvr_core/db/repository/base.go
@@ -4,28 +4,25 @@ import (
 	"nvr_core/db/models"
 	"nvr_core/logger"
 	"strings"
-	"time"
 )
 
 var LOG = logger.NewLogger("[nvr_core][db][repository]")
 
 // Format:
-// JoinSetFieldsClause("UPDATE table SET", updates)
+// JoinSetFieldsClause("UPDATE table SET", updates, true)
 func JoinSetFieldsClause(queryPrefix string, updates models.PartialUpdateInterfaces, withUpdateAt bool) (string, []interface{}) {
 
 	var args []interface{}
 	var setClauses []string
 
+	if withUpdateAt {
+		updates.ApplyUpdatedAt()
+	}
+
 	// Iterate through the safely constructed map
 	for col, val := range updates {
 		setClauses = append(setClauses, col+" = ?")
 		args = append(args, val)
-	}
-
-	if withUpdateAt {
-		// Always enforce the updated_at timestamp
-		setClauses = append(setClauses, "updated_at = ?")
-		args = append(args, time.Now().Unix())
 	}
 
 	// Stitch the query together safely
